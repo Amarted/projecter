632995d51f5912ba06d73c844a9b3fa59bb30916
massakra <demnarctis@gmail.com>
2015-10-11 12:32:47 +0400

    Modified methods structure. Implemented async adding and editing methods

--- WebContent/lib/app/js/projects.page.js
+++ WebContent/lib/app/js/projects.page.js
@@ -1,13 +1,83 @@
-var buttons = {
-		deleteEntity: $( ".entity .delete" ),
-		editEntity: $( ".entity .edit" ),
-		createEntity: $("#create")
-}
+/*
+ * Project page
+ */
 
 var projectFields = [ "id", "name", "description" ];
 var clearProjectData = { id: null, name: "", description: "" };
 var editWindow = new App.Class.Window( "editProject", true );
-editWindow.Hide();
+var projectsList = { 
+	element: $( "#projects .list" ),
+	editedRow: null
+	};
+
+
+/**
+ * Adds row to UI
+ * @return Object Created row
+ */
+projectsList.Add = function( project ) {
+	/**
+	 * TODO create row in UI
+	 */
+	var templ = '<tr class="entity project">';
+	templ += '	<td class="name"><a href="' + App.Base + 'tasks?project=' + project.id + '">' + project.name + '</a></td>';
+	templ += '	<td class="description">' + project.name + '</td>';
+	templ += '	<td class="controls">';
+	templ += '	<div class="data">';
+	templ += '		<input class="id" type="hidden" name="id" value="' + project.id + '" />';
+	templ += '	</div>';
+	templ += '	<button class="edit" title="Редактировать">Редактировать</button>';
+	templ += '	<button class="delete" type="submit" title="Удалить">X</button>	';				
+	templ += '	</td>';
+	templ += '</tr>';
+	var row = $( templ );
+	projectsList.element.append( row );
+	return row;
+}
+/**
+ * Update edited row by new data
+ */
+projectsList.UpdateEdited = function( newData ) {
+	var project = new App.Class.Structure( projectsList.editedRow, projectFields );
+	project.SetAllData( newData );
+}
+/**
+ * Delete project from UI and from the server
+ */
+projectsList.Delete = function( projectRow ) {
+	var project = new App.Class.Structure( projectRow, projectFields );
+	var id = project.Get("id");
+	var data = {
+		action: "delete",
+		id: id
+	};
+	App.Request( "projects", data, "POST" ).done( function( response ) {
+		if ( response.status == "error" ) return;
+		
+		// Remove row from UI
+		projectRow.remove();
+		
+	});
+}
+/** Load projects from server */
+projectsList.Load = function( filter ) {
+	var data = {
+		action: "getall",
+		filter: filter || null
+	};
+
+	App.Request( "projects", data, "GET" ).done( function( response ) {
+		if ( response.status == "error" ) return;
+		
+		// Add each row to UI
+		for ( var i in response ) {
+			var project = response[i]
+			projectsList.Add( project );
+		}
+	});
+}
+
+
 editWindow.form = { 
 	element: $("#editProject"),
 	statuses: {
@@ -19,54 +89,83 @@ editWindow.form.status = editWindow.form.statuses.CREATE;
 editWindow.form.buttons = {
 	submit: editWindow.form.element.find(".controls .submit")
 }
-editWindow.form.data = new App.Class.Structure( $("#editProject form") , projectFields);
+editWindow.form.data = new App.Class.Structure($("#editProject form") , projectFields);
 
-buttons.createEntity.on( "click", function( event ) {
-	editWindow.form.buttons.submit.text("Создать");
-	editWindow.form.data.SetAllData( clearProjectData );
-	editWindow.form.status = editWindow.form.statuses.CREATE
-	editWindow.SetTitle("Создание проекта");
-	editWindow.Show();
-});
-
-buttons.deleteEntity.on( "click", function( event ) {
-	if ( ! confirm( "Удалить проект? Вы сможете восстановить его на странице архива." )) {
-		return;
+/** Submit method */
+editWindow.form.Submit = function() {
+	
+	var project = editWindow.form.data.GetAllData();
+	 
+	if ( editWindow.form.status ===  editWindow.form.statuses.CREATE ) {
+		editWindow.form.Create( project );
+	} else if ( editWindow.form.status ===  editWindow.form.statuses.EDIT ) {
+		editWindow.form.Save( project );
 	}
-	var entityRow = $( event.target ).parents( "tr" );
-	var entity = new App.Class.Structure( entityRow, projectFields );
-	var id = entity.Get("id");
+}
+/** Create new project */
+editWindow.form.Create = function( project ) {
 	var data = {
-		action: "delete",
-		id: id
-	};
-	App.Request( "projects", data, "post" ).done( function( response ) {
-		if ( ! response.status ) {
-			alert( "Неверный ответ сервера" )
+			action: "create",
+			project: project
+		};
+	/**
+	 * TODO Server handling
+	 */
+	return App.Request( "projects", project, "POST" ).done( function( response ){ 
+		if ( response.status == "error" ) return;
+		// Add to UI
+		projectsList.Add( project );
+	});
+}
+/** Save project */
+editWindow.form.Save = function( project ) {
+	
+	var data = {
+			action: "save",
+			project: project
+		};
+	/**
+	 * TODO Server handling
+	 */
+	return App.Request( "projects", project, "POST" ).done( function( response ) { 
+		if ( response.status == "error" ) return;
+		projectsList.UpdateEdited( project );
+	});
+}
+
+// Creating project
+$( "#create").on( "click", function( event ) {
+	
+	editWindow.form.data.SetAllData( clearProjectData );
+	
+	editWindow.SetTitle("Создание проекта");
+	editWindow.form.buttons.submit.text( "Создать" );
+	editWindow.form.status = editWindow.form.statuses.CREATE;
+	
+	editWindow.Show();
+})
+
+// Operations
+$( ".entity")	
+	// Edit project
+	.on( "click", ".edit", function( event ) {	
+		var entityRow = $( event.target ).parents( "tr" );	 
+		var entity = new App.Class.Structure( entityRow, projectFields );	
+		editWindow.form.data.SetAllData( entity.GetAllData() );
+	
+		editWindow.SetTitle("Редактирование проекта");
+		editWindow.form.buttons.submit.text( "Сохранить" );
+		editWindow.form.status = editWindow.form.statuses.EDIT;
+		
+		editWindow.Show();
+	})
+	// Delete project
+	.on( "click", ".delete", function( event ) {
+		if ( ! confirm( "Удалить проект? Вы сможете восстановить его на странице архива." )) {
 			return;
 		}
-		if (  response.status == "ok" ) {
-			// remove row from UI
-			entityRow.remove();
-		} else if ( response.status == "error") {	
-			// Show error message
-			var errorMessage = response.message ? response.message : "Неизвестная ошибка";
-			alert( errorMessage );			
-		}
+		var entityRow = $( event.target ).parents( "tr" );
+		projectsList.Delete( entityRow );
+		
 	});
-});
 
-buttons.editEntity.on( "click", function( event ) {
-	/**
-	 * TODO
-	 */
-	var entityRow = $( event.target ).parents( "tr" );
-	var entity = new App.Class.Structure( entityRow, projectFields );
-	console.log(projectFields)
-	console.log(entity.GetAllData())
-	editWindow.form.data.SetAllData( entity.GetAllData() );
-	editWindow.form.buttons.submit.text("Сохранить");
-	editWindow.form.status = editWindow.form.statuses.EDIT
-	editWindow.SetTitle("Редактирование проекта");
-	editWindow.Show();
-});
